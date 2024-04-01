9985114527e6e001476eb4c178e73278b6e0e309
semual <semuall0108@gmail.com>
2024-03-31 22:57:13 -0700

    created a basic project to understand gsap

---
+++ gsap/basic/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+app.use(express.json());
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/html/index.html')
+})
+
+app.listen(3000, () => {
+    console.log("LISTENING ON PORT 3000")
+})
---
+++ gsap/basic/public/js/index.js
@@ -0,0 +1,6 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    gsap.to('.box', {x:750, duration: 4})
+
+})
