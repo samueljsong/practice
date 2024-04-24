19c726da178924269e6ecb1bd415d28f8d5b30ed
samueljsong <semuall0108@gmail.com>
2024-04-24 00:51:44 -0700

    finished horizontal scroll

--- websites/moon/public/js/index.js
+++ websites/moon/public/js/index.js
@@ -12,7 +12,8 @@ document.addEventListener("DOMContentLoaded", () => {
             pin: true,
             scrub: 1,
             start: 'top top',
-            end: 'bottom bottom'
+            end: 'bottom bottom',
+            pinSpacing: false
         }
     })
 
@@ -24,5 +25,24 @@ document.addEventListener("DOMContentLoaded", () => {
 
     t1.to('#t1', {y: -(TITLE_CHANGE_AMT/12), opacity: 1}, 0);
     t1.to('#t3', {y: (TITLE_CHANGE_AMT/12), opacity: 1}, 0);
-    t1.to('#moon', {scale: 1.5, rotation: 45}, 0)
+    t1.to('#moon', {scale: 1.45, rotation: 45}, 0)
+
+    ScrollTrigger.create({
+        trigger: ".information", // Element that triggers the animation
+        start: "top top", // Animation starts when top of trigger element reaches top of viewport
+        end: "bottom bottom", // Animation ends when bottom of trigger element reaches top of viewport
+        pin: true, // Pin the trigger element during the animation
+        pinSpacing: false, // Disable automatic spacer element for pinning
+        // scrub: 1,
+        onUpdate: self => {
+            const scrollContainer = document.querySelector(".information-container");
+            const scrollProgress = self.progress; // Get current progress of the ScrollTrigger
+            const maxScroll = scrollContainer.scrollWidth - scrollContainer.clientWidth; // Calculate maximum scroll distance
+    
+            gsap.to(scrollContainer, {
+                x: -maxScroll * scrollProgress, // Calculate horizontal scroll distance based on progress
+                ease: "none" // Linear scrolling
+            });
+        }
+    });
 })
