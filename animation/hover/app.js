146ecc980af6c98dad3888f3ee9ef15f0860bdaf
samueljsong <semuall0108@gmail.com>
2024-04-24 20:58:56 -0700

    attempted hover effect

---
+++ animation/hover/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/public/html/index.html")
+})
+
+app.listen(3000, () => {
+    console.log("HOVER ON 3000")
+})
---
+++ animation/hover/public/js/index.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+    
+    let t1 = gsap.timeline();
+    t1.to('.base', {
+        opacity: 1,
+        stagger: 0.1
+    })
+
+})
