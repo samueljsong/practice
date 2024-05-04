387998a14881e692f64455cd6e6a25a5a460b057
samueljsong <semuall0108@gmail.com>
2024-05-04 01:01:31 -0700

    fixed stagger animation

--- websites/practice1/public/js/hero.js
+++ websites/practice1/public/js/hero.js
@@ -2,70 +2,47 @@
 
 document.addEventListener("DOMContentLoaded", () => {
 
-    // Gets window width for spacing
-    let windowWidth = window.innerWidth;
-    console.log(windowWidth)
+    const t1 = gsap.timeline();
 
-    window.addEventListener('resize', () => {
-        windowWidth = window.innerWidth
-    })
+    const textArray = document.querySelectorAll('.text-wrapper h1');
+    
+    t1.from('nav', {
+        y: -20,
+        duration: 0.75,
+        opacity: 0
+    },0)
 
-    // Grabs all the heading text
-    let heading = document.querySelector('.heading').textContent;
-    let headingArray = heading.split(" ");
+    t1.from('.consultation', {
+        y: -20,
+        duration: 0.75,
+        opacity: 0
+    },0)
 
-    let newHTML = ''
+    t1.from('.logo', {
+        y: -20,
+        duration: 0.75,
+        opacity: 0
+    },0)
 
-    headingArray.forEach((word) => {
-        if (word.toLowerCase() === 'forward'){
-            newHTML += `<span>${word}</span><br>`
-        }
-        else{
-            newHTML += `<span>${word}</span>`
-        };
-    })
+    t1.from('.menu', {
+        y: -20,
+        duration: 0.75,
+        opacity: 0
+    },0)
 
-    document.querySelector('.heading').innerHTML = newHTML
+    t1.from('.message', {
+        y: -20,
+        duration: 0.75,
+        opacity: 0
+    },0)
 
-    const allwords = document.querySelectorAll('.heading span')
-  
-    let firstline = 120;
-    let secondline = 120;
-
-    allwords.forEach((span, index) => {
-        if(index < 4){
-            firstline += span.offsetWidth;
-            console.log(span.offsetWidth)
-        }
-        else{
-            secondline += span.offsetWidth
-        }
-    })
-
-    let firstlineStart = (windowWidth - firstline) / 2;
-    let secondlineStart = (windowWidth - secondline) / 2;
-
-    allwords.forEach((span, index) => {
-        if(index < 4){
-            span.style.left = `${firstlineStart}px`
-            firstlineStart = firstlineStart + span.clientWidth + 40;
-        }
-        else{
-            span.style.left = `${secondlineStart}px`
-            secondlineStart = secondlineStart + span.clientWidth + 40;
-        }
-    })
-
-
-
-
-    let t1 = gsap.timeline();
-
-    t1.to(allwords, {
-        opacity: 1,
-        marginTop: -50,
+    t1.to(textArray, {
+        opacity:1,
         duration: 0.5,
-        stagger: 0.2
-    })
+        stagger: 0.15,
+        y: -20
+    }, 0.5)
+
+    
 
 })
